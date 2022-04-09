1ebc001565818be35c930cb977754dcf8cccdaeb
mehdiparastar <parastar.mehdi@gmail.com>
2022-04-09 14:55:51 +0430

    up-to-s-4-01

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,7 @@
 import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../tasks.model';
 
+
 export class GetTasksFilterDTO {
   @IsOptional()
   @IsEnum(TaskStatus)
